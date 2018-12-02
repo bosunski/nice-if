fdbe3153f37496033737f1f38b8a33b5c985eb12
Olatunbosun Egberinde <bosunski@gmail.com>
2018-12-02 18:24:07 +0100

    Release 0.0.2

--- functions/condition.php
+++ functions/condition.php
@@ -2,6 +2,12 @@
 
 use Neater\Condition\Condition;
 
+/**
+ * Provided a public accessor for the Condition class
+ * @param $task
+ *
+ * @return Condition
+ */
 function perform($task) {
 	$condition = new Condition;
 
