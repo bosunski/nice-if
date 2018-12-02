8f2093c6e51b689c1989211cc43989c80265e2f7
Olatunbosun Egberinde <bosunski@gmail.com>
2018-12-02 18:20:45 +0100

    Added perform function

--- functions/condition.php
+++ functions/condition.php
@@ -1,8 +1,9 @@
 <?php
 
-/*
- * This file is part of <package name>.
- *
- * This source file is subject to the license that is bundled
- * with this source code in the file LICENSE.
- */
+use Neater\Condition\Condition;
+
+function perform($task) {
+	$condition = new Condition;
+
+	return $condition->perform($task);
+}
