114779d8d5284a5a5fd9fd214b14acdcb0fd8300
Olatunbosun Egberinde <bosunski@gmail.com>
2018-12-02 17:41:58 +0100

    Release 0.0.1

--- functions/condition.php
+++ functions/condition.php
@@ -1,1 +1,8 @@
 <?php
+
+/*
+ * This file is part of <package name>.
+ *
+ * This source file is subject to the license that is bundled
+ * with this source code in the file LICENSE.
+ */
--- src/Condition.php
+++ src/Condition.php
@@ -1,92 +1,104 @@
 <?php
 
-namespace Vendor\Package;
+/*
+ * This file is part of <package name>.
+ *
+ * This source file is subject to the license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Neater\Condition;
 
 use Closure;
 
 class Condition
 {
-	/**
-	 * @var callable
-	 */
-	private $supposedAction;
+    /**
+     * @var callable
+     */
+    private $supposedAction;
 
-	/**
-	 * @var callable
-	 */
-	private $fallbackAction;
+    /**
+     * @var callable
+     */
+    private $fallbackAction;
 
-	/**
-	 * @var mixed
-	 */
-	private $actualValue;
+    /**
+     * @var mixed
+     */
+    private $actualValue;
 
-	/**
-	 * @var mixed
-	 */
-	private $expectedValue;
+    /**
+     * @var mixed
+     */
+    private $expectedValue;
 
-	/**
-	 * Return Value of last call, if any
-	 * @var mixed
-	 */
-	private $lastReturn = null;
+    /**
+     * Return Value of last call, if any.
+     *
+     * @var mixed
+     */
+    private $lastReturn = null;
 
-	/**
-	 * Flags the call to the intended action
-	 * @var bool
-	 */
-	private $supposedActionCalled = false;
+    /**
+     * Flags the call to the intended action.
+     *
+     * @var bool
+     */
+    private $supposedActionCalled = false;
 
-	public function perform(callable $something)
-	{
-		$this->supposedAction = Closure::fromCallable($something);
+    public function perform(callable $something)
+    {
+        $this->supposedAction = Closure::fromCallable($something);
 
-		return $this;
-	}
+        return $this;
+    }
 
-	public function once($value)
-	{
-		$this->actualValue = $value;
-		return $this;
-	}
+    public function once($value)
+    {
+        $this->actualValue = $value;
 
-	public function is($expected)
-	{
-		$this->expectedValue = $expected;
-		$this->run();
-		return $this;
-	}
+        return $this;
+    }
 
-	/**
-	 * Do something else
-	 *
-	 * @param callable $somethingElse
-	 */
-	public function otherwiseDo(callable $somethingElse)
-	{
-		$this->fallbackAction = Closure::fromCallable($somethingElse);
+    public function is($expected)
+    {
+        $this->expectedValue = $expected;
+        $this->run();
 
-		$this->run();
-	}
+        return $this;
+    }
 
-	public function call(callable $callable)
-	{
-		$this->lastReturn = call_user_func($callable);
-	}
+    /**
+     * Do something else.
+     *
+     * @param callable $somethingElse
+     */
+    public function otherwiseDo(callable $somethingElse)
+    {
+        $this->fallbackAction = Closure::fromCallable($somethingElse);
 
-	/**
-	 * Runs the Whole evaluation
-	 */
-	public function run()
-	{
-		if (!$this->supposedActionCalled && $this->actualValue === $this->expectedValue) {
-			$this->call($this->supposedAction);
+        $this->run();
+    }
 
-			$this->supposedActionCalled = true;
-		} else {
-			if ($this->fallbackAction instanceof Closure)
-				$this->call( $this->fallbackAction );
-		}
-	}
+    public function call(callable $callable)
+    {
+        $this->lastReturn = call_user_func($callable);
+    }
+
+    /**
+     * Runs the Whole evaluation.
+     */
+    public function run()
+    {
+        if (!$this->supposedActionCalled && $this->actualValue === $this->expectedValue) {
+            $this->call($this->supposedAction);
+
+            $this->supposedActionCalled = true;
+        } else {
+            if ($this->fallbackAction instanceof Closure) {
+                $this->call($this->fallbackAction);
+            }
+        }
+    }
 }
